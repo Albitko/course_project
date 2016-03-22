273e93b89088c8829cda8787e068e6a1be9c29df
Albitkov <alxsmrn@yandex.ru>
2016-03-22 18:09:46 +0400

    Более менее рабочая версия, н больше 6 блоков не ищет

--- controller.php
+++ controller.php
@@ -3,15 +3,19 @@
 require 'simple_html_dom.php';//Подключаем библиотеку
 require 'db.php';
 require 'functions.php';
-
+$x='1';
 $old_news=SelectNews();
-$new_news=ParseIt();
-var_dump($old_news);
-var_dump($new_news);
-
-while ($old_news!=$new_news) {
+$new_news=ParseIt($x);
+InsertNews($new_news);
+//var_dump($old_news);
+//var_dump($new_news);
+for ($i=1;$old_news!=$new_news; $i++) { //БОЛЬШЕ 6 ОБЪЕКТОВ НЕ ПРЕНИМАЕТ
+	//ВЫВОДИТ 6 И ОШИБКИ
+	$NEWS[]=$new_news;
 	$old_news=$new_news;
-	$new_news=//следующая новост???
+	$new_news=ParseIt($i);
 }
+
+print_r($NEWS);
 
  ?>
