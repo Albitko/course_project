df7157a338b30f30c5d1d5ae4998b07e3ca0be51
Albitkov <alxsmrn@yandex.ru>
2016-03-22 14:12:38 +0400

    Встретился с проблемой нехватки новой функции

--- controller.php
+++ controller.php
@@ -1,10 +1,17 @@
 <?php 
 
-require_once 'simple_html_dom.php';//Подключаем библиотеку
+require 'simple_html_dom.php';//Подключаем библиотеку
 require 'db.php';
 require 'functions.php';
 
 $old_news=SelectNews();
 $new_news=ParseIt();
+var_dump($old_news);
+var_dump($new_news);
+
+while ($old_news!=$new_news) {
+	$old_news=$new_news;
+	$new_news=//следующая новост???
+}
 
  ?>
