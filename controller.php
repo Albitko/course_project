a9c9cd4120e3cca9f88353ee83b758b3055cd600
Albitkov <alxsmrn@yandex.ru>
2016-03-25 19:33:42 +0400

    Рабочая функция получения новых новостей

--- controller.php
+++ controller.php
@@ -3,19 +3,25 @@
 require 'simple_html_dom.php';//Подключаем библиотеку
 require 'db.php';
 require 'functions.php';
-$x='1';
-$old_news=SelectNews();
-$new_news=ParseIt($x);
-InsertNews($new_news);
-//var_dump($old_news);
-//var_dump($new_news);
-for ($i=1;$old_news!=$new_news; $i++) { //БОЛЬШЕ 6 ОБЪЕКТОВ НЕ ПРЕНИМАЕТ
-	//ВЫВОДИТ 6 И ОШИБКИ
-	$NEWS[]=$new_news;
-	$old_news=$new_news;
-	$new_news=ParseIt($i);
-}
 
-print_r($NEWS);
+$a='h1.title';
+$b='a.post_title';
+$c='https://habrahabr.ru/hub/infosecurity/all/';
+$d='4';
+$n='1';
+
+$old_news=Parse($a,$b,$c,$d);
+//var_dump($old_news);
+
+$new_news=Parse($a,$b,$c,$n);
+//Рабочая функция выведения всех новых заголовков
+do {
+	$NEWS[]=$new_news;
+	$n++;
+	$new_news=Parse($a,$b,$c,$n);
+} while ($old_news!=$new_news);
+InserNews($NEWS[0]);
+	var_dump($NEWS);
+
 
  ?>
