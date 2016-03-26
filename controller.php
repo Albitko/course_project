ef75371fd8939274e1fb78f295b3b2b1d7322fdd
Albitkov <alxsmrn@yandex.ru>
2016-03-26 15:17:27 +0400

    Написал основную функцию для парсинга разницы, между старым сообщением и новым

--- controller.php
+++ controller.php
@@ -4,24 +4,34 @@ require 'simple_html_dom.php';//Подключаем библиотеку
 require 'db.php';
 require 'functions.php';
 
-$a='h1.title';
-$b='a.post_title';
-$c='https://habrahabr.ru/hub/infosecurity/all/';
-$d='4';
-$n='1';
+$sel_from='h1.title';
+$sel_as='a.post_title';
+$url='https://habrahabr.ru/hub/infosecurity/all/';
+$number='1';
+$for_example='5';
+//$old_news=SelectLastNews();
+$old_news=Parse($sel_from,$sel_as,$url,$for_example);
+$new_news=Parse($sel_from,$sel_as,$url,$number);
 
-$old_news=Parse($a,$b,$c,$d);
-//var_dump($old_news);
-
-$new_news=Parse($a,$b,$c,$n);
 //Рабочая функция выведения всех новых заголовков
+while ($old_news!=$new_news){
+	InsertNews($new_news);
+	$number++;
+	$new_news=Parse($sel_from,$sel_as,$url,$number);
+} 
+
+$NEWS=SelectManyNews($number);
+print_r($NEWS);
+/*
 do {
 	$NEWS[]=$new_news;
 	$n++;
 	$new_news=Parse($a,$b,$c,$n);
 } while ($old_news!=$new_news);
+
 InserNews($NEWS[0]);
 	var_dump($NEWS);
 
+*/
 
  ?>
