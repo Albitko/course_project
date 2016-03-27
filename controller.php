d240f04afe9ebeccfad604f7b456d84cd6c8bae6
Albitkov <alxsmrn@yandex.ru>
2016-03-27 13:27:03 +0400

    Расширил функцию см db

--- controller.php
+++ controller.php
@@ -6,9 +6,9 @@ require 'functions.php';
 
 //Записать в БД новые записи с момента последней статьи
 function InsertNewNews($sel_from,$sel_as,$url,$number,
-	$old_news,$new_news){
+	$old_news,$new_news,$where){
 	while ($old_news!=$new_news){
-		InsertNews($new_news);
+		InsertNews($new_news,$where);
 		$number++;
 		$new_news=Parse($sel_from,$sel_as,$url,$number);
 	} 
@@ -23,6 +23,7 @@ $for_example='5';
 //$old_news=SelectLastNews();
 $old_news=Parse($sel_from,$sel_as,$url,$for_example);
 $new_news=Parse($sel_from,$sel_as,$url,$number);
+$where='title_news';
 /*
 //Рабочая функция выведения всех новых заголовков
 while ($old_news!=$new_news){
@@ -31,8 +32,12 @@ while ($old_news!=$new_news){
 	$new_news=Parse($sel_from,$sel_as,$url,$number);
 } */
 InsertNewNews($sel_from,$sel_as,$url,$number,
-	$old_news,$new_news);
+	$old_news,$new_news,$where);
 $NEWS=SelectManyNews(4);
 print_r($NEWS);
-
+/*
+$a='.shortcuts_item';
+$b='.html_format';
+$x=Parse($a,$b,$url,$number);
+var_dump($x);*/
  ?>
