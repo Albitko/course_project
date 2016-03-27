bb3d0982023d017b5a37276001e13c1de4bcda7b
Albitkov <alxsmrn@yandex.ru>
2016-03-27 13:09:20 +0400

    Сделал основную функцию

--- controller.php
+++ controller.php
@@ -4,6 +4,17 @@ require 'simple_html_dom.php';//Подключаем библиотеку
 require 'db.php';
 require 'functions.php';
 
+//Записать в БД новые записи с момента последней статьи
+function InsertNewNews($sel_from,$sel_as,$url,$number,
+	$old_news,$new_news){
+	while ($old_news!=$new_news){
+		InsertNews($new_news);
+		$number++;
+		$new_news=Parse($sel_from,$sel_as,$url,$number);
+	} 
+}
+
+
 $sel_from='h1.title';
 $sel_as='a.post_title';
 $url='https://habrahabr.ru/hub/infosecurity/all/';
@@ -12,15 +23,16 @@ $for_example='5';
 //$old_news=SelectLastNews();
 $old_news=Parse($sel_from,$sel_as,$url,$for_example);
 $new_news=Parse($sel_from,$sel_as,$url,$number);
-
+/*
 //Рабочая функция выведения всех новых заголовков
 while ($old_news!=$new_news){
 	InsertNews($new_news);
 	$number++;
 	$new_news=Parse($sel_from,$sel_as,$url,$number);
-} 
-
-$NEWS=SelectManyNews($number);
+} */
+InsertNewNews($sel_from,$sel_as,$url,$number,
+	$old_news,$new_news);
+$NEWS=SelectManyNews(4);
 print_r($NEWS);
 
  ?>
