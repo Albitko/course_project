52986c96e13c96bc68ce74e2fb746eac39868d19
Albitkov <alxsmrn@yandex.ru>
2016-04-05 14:09:33 +0400

    Стабильный вариант

--- controller.php
+++ controller.php
@@ -1,21 +1,34 @@
 <?php 
 
-require 'simple_html_dom.php';//Подключаем библиотеку
-require 'db.php';
-require 'functions.php';
+	require 'simple_html_dom.php';//Подключаем библиотеку
+	require 'oop_db.php';
+	require 'oop_funktions.php';
 
 //Записать в БД новые записи с момента последней статьи
-function InsertNewNews($sel_from_title,$sel_from_text,$sel_as_title,
+/**
+* Итоговый класс
+*/
+
+class ParsingWhatNeed extends Parse
+{
+	
+	function InsertParsingNews($sel_from_title,$sel_from_text,$sel_as_title,
 	$sel_as_text, $url,$number, $old_news,$new_news,$where_title,$where_text){
 	while ($old_news!=$new_news){
-		$text_news=Parse($sel_from_text,$sel_as_text,$url,$number);
+		$all=new InsertNews;
+		$text_news=parent::Parsing($sel_from_text,$sel_as_text,$url,$number);
 		//InsertNews($new_news,$where_title);
-		InsertAllNews($where_text,$where_title,$new_news,$text_news);
+		$all->InsertAllNews($where_text,$where_title,$new_news,$text_news);
 		$number++;
-		$new_news=Parse($sel_from_title,$sel_as_title,$url,$number);
+		$new_news=parent::Parsing($sel_from_title,$sel_as_title,$url,$number);
 	} 
 }
 
+
+}
+
+$newses=new Parse();
+$final=new ParsingWhatNeed();
 
 $sel_from_title='h1.title';
 $sel_as_title='a.post_title';
@@ -23,18 +36,18 @@ $url='https://habrahabr.ru/hub/infosecurity/all/';
 $number='1';
 $for_example='5';
 //$old_news=SelectLastNews();
-$old_news=Parse($sel_from_title,$sel_as_title,$url,$for_example);
-$new_news=Parse($sel_from_title,$sel_as_title,$url,$number);
+$old_news=$newses->Parsing($sel_from_title,$sel_as_title,$url,$for_example);
+$new_news=$newses->Parsing($sel_from_title,$sel_as_title,$url,$number);
 $where_title='title_news';
 $sel_from_text='.shortcuts_item';
 $sel_as_text='.html_format';
 $where_text='text_news';
 
 
-//InsertNewNews($sel_from_title,$sel_from_text,$sel_as_title,
-	//$sel_as_text, $url,$number, $old_news,$new_news,$where_title,$where_text);
-$XT=SelectManyNews(4);
-var_dump($XT);
+$final->InsertParsingNews	($sel_from_title,$sel_from_text,$sel_as_title,
+$sel_as_text, $url,$number, $old_news,$new_news,$where_title,$where_text);
+//$XT=SelectManyNews(4);
+//var_dump($XT);
 
 
  ?>
